d84f8b51c48dcea6beb3f4a3ed6d49ddd4125fbb
Wroztee <wroztee@gmail.com>
2024-10-29 11:56:58 +0100

    Added pdf image preview and scroll area

--- ui.py
+++ ui.py
@@ -1,12 +1,22 @@
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QSpinBox, QSizePolicy
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QSpinBox, QSizePolicy, QScrollArea, QMainWindow
+from PySide6.QtCore import QSize, Qt
+import pdf2image as pdf2img
+from PyPDF2 import PdfReader
+
 from main import merge_pdf, select_pdf_paths, select_out_path, get_page_count
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("PDF Merger")
         self.setGeometry(100, 100, 700, 400)
+
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.setCentralWidget(self.scroll_area)
+        self.scroll_widget = QWidget()
+        self.scroll_area.setWidget(self.scroll_widget)
 
         self.load_button = QPushButton("Load PDFs")
         self.load_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
@@ -21,7 +31,9 @@ class MainWindow(QWidget):
 
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(top_button_layout)
-        self.setLayout(self.main_layout)
+        self.main_layout.addStretch()
+
+        self.scroll_widget.setLayout(self.main_layout)
 
         self.item_widgets = []
 
@@ -60,35 +72,83 @@ class ItemRow(QWidget):
         self.path = path
         self.page_count = page_count
 
-        self.label = QLabel(f".../{path.split("/")[-1]}")
-        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        reader = PdfReader(path)
+
+        self.image = pdf2img.convert_from_path(path, dpi=20, first_page=0, last_page=1)[0]
+        self.image_label = QLabel(self)
+        self.image_label.setPixmap(self.image.toqpixmap().scaled(100, 150, Qt.KeepAspectRatio))
+        self.image_label.setScaledContents(True)
+
+        self.path_label = QLabel(f".../{path.split("/")[-1]}")
+        self.path_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        self.title_label = QLabel()
+        if "/Title" in reader.metadata and reader.metadata["/Title"] != "":
+            self.title = reader.metadata["/Title"]
+            self.title_label.setText("Title: " + self.title)
+
+        self.creator_label = QLabel()
+        if "/Creator" in reader.metadata and reader.metadata["/Creator"] != "":
+            self.creator = reader.metadata["/Creator"]
+            self.creator_label.setText("Creator: " + self.creator)
+
+        self.producer_label = QLabel()
+        if "/Producer" in reader.metadata and reader.metadata["/Producer"] != "":
+            self.producer = reader.metadata["/Producer"]
+            self.producer_label.setText("Producer: " + self.producer)
+
         self.start_page_spin_box = QSpinBox()
         self.start_page_spin_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.start_page_spin_box.setRange(1, page_count)
         self.start_page_spin_box.setValue(1)
         self.start_page_spin_box.valueChanged.connect(self.start_page_changed)
+
         self.end_page_spin_box = QSpinBox()
         self.end_page_spin_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.end_page_spin_box.setRange(1, page_count)
         self.end_page_spin_box.setValue(page_count)
         self.end_page_spin_box.valueChanged.connect(self.end_page_changed)
+
         self.up_button = QPushButton("↑")
         self.up_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.up_button.clicked.connect(self.move_item_up)
+
         self.down_button = QPushButton("↓")
         self.down_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.down_button.clicked.connect(self.move_item_down)
+
         self.remove_button = QPushButton("Remove")
         self.remove_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.remove_button.clicked.connect(self.remove_item)
 
+
         self.main_layout = QHBoxLayout()
-        self.main_layout.addWidget(self.label, 3)
-        self.main_layout.addWidget(self.start_page_spin_box, 1)
-        self.main_layout.addWidget(self.end_page_spin_box, 1)
-        self.main_layout.addWidget(self.up_button)
-        self.main_layout.addWidget(self.down_button)
-        self.main_layout.addWidget(self.remove_button)
+
+        self.main_layout.addWidget(self.image_label, 1)
+
+        self.info_layout = QVBoxLayout()
+        self.info_layout.addWidget(self.path_label)
+        self.info_layout.addWidget(self.title_label)
+        self.info_layout.addWidget(self.creator_label)
+        self.info_layout.addWidget(self.producer_label)
+        self.info_layout.addStretch()
+        self.main_layout.addLayout(self.info_layout, 3)
+
+        self.page_range_layout = QVBoxLayout()
+        self.page_range_layout.addStretch()
+        self.page_range_layout.addWidget(self.start_page_spin_box)
+        self.page_range_layout.addWidget(self.end_page_spin_box)
+        self.main_layout.addLayout(self.page_range_layout, 1)
+        self.page_range_layout.addStretch()
+
+        self.move_page_layout = QVBoxLayout()
+        self.move_page_layout.addStretch()
+        self.move_page_layout.addWidget(self.up_button)
+        self.move_page_layout.addWidget(self.down_button)
+        self.main_layout.addLayout(self.move_page_layout, 1)
+        self.move_page_layout.addStretch()
+
+        self.main_layout.addWidget(self.remove_button, 1)
 
         self.setLayout(self.main_layout)
 
